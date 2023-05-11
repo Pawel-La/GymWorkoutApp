ec5190f0d7e7a2c25279cadf0e1f0d6e64ff10d7
Paweł <pawellamza@gmail.com>
2023-05-11 14:02:36 +0200

    added weight property and weight setter

--- ExerciseSet.py
+++ ExerciseSet.py
@@ -6,6 +6,18 @@ class ExerciseSet:
         self.expected_number_of_reps = expected_number_of_reps
         self.number_of_completed_reps = 0
 
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, weight):
+        if not isinstance(weight, (int, float)):
+            raise Exception("Weight must be of numeric type!")
+        if weight <= 0:
+            raise Exception("Weight can't be a negative number!")
+        self._weight = weight
+
     def summary(self) -> str:
         return f"Weight: {self.weight}\n" \
                f"Expected number of reps: {self.expected_number_of_reps}\n" \
