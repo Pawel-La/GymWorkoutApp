16edfd59ca4e05b27f7382383ecc1b394cf5a53c
Paweł <pawellamza@gmail.com>
2023-05-11 16:44:45 +0200

    added tests for expected_number_of_reps setter

--- test_ExerciseSet.py
+++ test_ExerciseSet.py
@@ -26,3 +26,21 @@ def test_correct_weight_setter(exercise_set):
     weight = 100.75
     exercise_set.weight = weight
     assert exercise_set.weight == weight
+
+
+def test_non_integer_expected_number_of_reps_setter(exercise_set):
+    expected_number_of_reps = 1.244
+    with pytest.raises(Exception):
+        exercise_set.expected_number_of_reps = expected_number_of_reps
+
+
+def test_negative_expected_number_of_reps_setter(exercise_set):
+    expected_number_of_reps = -100
+    with pytest.raises(Exception):
+        exercise_set.expected_number_of_reps = expected_number_of_reps
+
+
+def test_correct_expected_number_of_reps_setter(exercise_set):
+    expected_number_of_reps = 10
+    exercise_set.expected_number_of_reps = expected_number_of_reps
+    assert exercise_set.expected_number_of_reps == expected_number_of_reps
