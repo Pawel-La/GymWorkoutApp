e00d69f602a4e5621639b923952be4bcbba29947
Paweł <pawellamza@gmail.com>
2023-05-11 17:17:07 +0200

    added number_of_completed_reps property with setter

--- ExerciseSet.py
+++ ExerciseSet.py
@@ -30,6 +30,21 @@ class ExerciseSet:
             raise Exception("Expected number of reps must be positive!")
         self._expected_number_of_reps = expected_number_of_reps
 
+    @property
+    def number_of_completed_reps(self):
+        return self._number_of_completed_reps
+
+    @number_of_completed_reps.setter
+    def number_of_completed_reps(self, number_of_completed_reps):
+        if not isinstance(number_of_completed_reps, int):
+            raise Exception("Number of completed reps must be an integer!")
+        if number_of_completed_reps < 0:
+            raise Exception("Number of completed reps can't be negative!")
+        if hasattr(self, 'number_of_completed_reps') and \
+                self.number_of_completed_reps > 0:
+            raise Exception("Number of completed reps has been already set!")
+        self._number_of_completed_reps = number_of_completed_reps
+
     def summary(self) -> str:
         return f"Weight: {self.weight}\n" \
                f"Expected number of reps: {self.expected_number_of_reps}\n" \
