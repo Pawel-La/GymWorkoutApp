30b21a10ae1fb42fb85a92e9b66381ce2f11b52a
Paweł <pawellamza@gmail.com>
2023-05-11 14:10:16 +0200

    added tests for weight in ExerciseSet

--- test_ExerciseSet.py
+++ test_ExerciseSet.py
@@ -8,3 +8,21 @@ def exercise_set() -> ExerciseSet:
     expected_number_of_reps = 12
     exercise_set = ExerciseSet(weight, expected_number_of_reps)
     return exercise_set
+
+
+def test_non_numeric_weight_setter(exercise_set):
+    weight = "a"
+    with pytest.raises(Exception):
+        exercise_set.weight = weight
+
+
+def test_negative_number_weight_setter(exercise_set):
+    weight = -100
+    with pytest.raises(Exception):
+        exercise_set.weight = weight
+
+
+def test_correct_weight_setter(exercise_set):
+    weight = 100.75
+    exercise_set.weight = weight
+    assert exercise_set.weight == weight
