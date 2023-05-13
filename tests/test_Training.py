08882374f9e4ec514418e0a6d3f4edd37632eaeb
Paweł <pawellamza@gmail.com>
2023-05-13 17:31:41 +0200

    added tests for add_one_exercise

--- tests/test_Training.py
+++ tests/test_Training.py
@@ -22,7 +22,7 @@ def training() -> Training:
 
 
 def test_exercises_initialized(training):
-    assert training.exercises == []
+    assert training.dict_of_exercises == dict()
 
 
 def test_correct_date_setter(training):
@@ -31,48 +31,49 @@ def test_correct_date_setter(training):
     assert date == training.date
 
 
-def test_incorrect_date_setter(training):
+def test_incorrect_class_object_date_setter(training):
     date = 10
     with pytest.raises(Exception):
         training.date = date
     assert training.date != date
 
 
-def test_empty_list_exercises_setter(training):
-    exercises = []
-    training.exercises = exercises
-    assert exercises == training.exercises
+# def test_correct_exercises_list_setter(training):
+#     exercises = [Exercise(name="Pull up", number_of_exercise_sets=5),
+#                  Exercise(name="Push up", number_of_exercise_sets=4)]
+#     training.exercises = exercises
+#     assert exercises == training.exercises
 
 
-def test_correct_exercises_list_setter(training):
-    exercises = [Exercise(name="Pull up", number_of_exercise_sets=5),
-                 Exercise(name="Push up", number_of_exercise_sets=4)]
-    training.exercises = exercises
-    assert exercises == training.exercises
+# def test_incorrect_exercises_list_setter(training):
+#     exercises = [Exercise(name="Pull up", number_of_exercise_sets=5),
+#                  Exercise(name="Push up", number_of_exercise_sets=4),
+#                  512412]
+#
+#     with pytest.raises(Exception):
+#         training.exercises = exercises
+#     assert training.exercises != exercises
 
 
-def test_incorrect_exercises_list_setter(training):
-    exercises = [Exercise(name="Pull up", number_of_exercise_sets=5),
-                 Exercise(name="Push up", number_of_exercise_sets=4),
-                 512412]
-
-    with pytest.raises(Exception):
-        training.exercises = exercises
-    assert training.exercises != exercises
-
-
-def test_correct_add_exercise(training):
-    exercises = training.exercises
+def test_correct_add_one_exercise(training):
     exercise = Exercise(name="Pull up", number_of_exercise_sets=5)
-
-    training.add_exercise(exercise)
-    exercises.append(exercise)
-
-    assert training.exercises == exercises
+    training.add_one_exercise(exercise)
+    assert training.dict_of_exercises[exercise.name] == exercise
 
 
-def test_incorrect_add_exercise(training):
+def test_incorrect_class_object_add_one_exercise(training):
     exercise = 10
     with pytest.raises(Exception):
-        training.add_exercise(exercise)
-    assert training.exercises != exercise
+        training.add_one_exercise(exercise)
+
+
+def test_already_exists_add_one_exercise(training):
+    exercise1 = Exercise(name="Pull up", number_of_exercise_sets=5)
+    exercise2 = Exercise(name="Pull up", number_of_exercise_sets=4)
+    training.add_one_exercise(exercise1)
+    with pytest.raises(Exception):
+        training.add_one_exercise(exercise1)
+    with pytest.raises(Exception):
+        training.add_one_exercise(exercise2)
+    assert len(training.dict_of_exercises) == 1
+    assert training.dict_of_exercises[exercise1.name] == exercise1
