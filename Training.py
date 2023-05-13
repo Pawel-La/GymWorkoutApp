335acc7f54c1380abd5be0119f07563a6adb04e3
Paweł <pawellamza@gmail.com>
2023-05-13 17:23:22 +0200

    * added add_one_exercise method, * created new property dict_of_exercises, * made setter for property dict_of_exercises, so that it is possible to set only once

--- Training.py
+++ Training.py
@@ -1,17 +1,31 @@
 from datetime import datetime
-from typing import Any, List
-# from multipledispatch import dispatch
+from typing import Any, List, Dict
 from Exercise import Exercise
+
+
+# def get_dict_of_exercises_names_and_exercises_from_exercises_list(
+#         list_of_exercises: List[Exercise]) -> Dict[str, Exercise]:
+#     dict_of_exercises_names_and_exercises = dict()
+#
+#     for exercise in list_of_exercises:
+#         if not isinstance(exercise, Exercise):
+#             raise Exception("All exercises must be Exercise class objects!")
+#         if exercise.name in dict_of_exercises_names_and_exercises:
+#             raise Exception("Different exercises in the same training must "
+#                             "have different names!")
+#         dict_of_exercises_names_and_exercises[exercise.name] = exercise
+#
+#     return dict_of_exercises_names_and_exercises
 
 
 class Training:
     def __init__(self,
                  date: datetime = datetime.now(),
-                 exercises: List[Exercise] = None) -> None:
+                 list_of_exercises: List[Exercise] = None) -> None:
         self.date = date
-        if exercises is None:
-            exercises = []
-        self.exercises = exercises
+        self.dict_of_exercises = dict()
+        # if list_of_exercises is not None:
+        #     self.add_multiple_exercises(list_of_exercises)
 
     @property
     def date(self) -> datetime:
@@ -24,22 +38,28 @@ class Training:
         self._date = date
 
     @property
-    def exercises(self) -> List[Exercise]:
-        return self._exercises
+    def dict_of_exercises(self) -> Dict[str, Exercise]:
+        return self._dict_of_exercises
 
-    @exercises.setter
-    def exercises(self, exercises: Any) -> None:
-        if not isinstance(exercises, List):
-            raise Exception("Exercises must be a list!")
+    @dict_of_exercises.setter
+    def dict_of_exercises(self, dict_of_exercises: Dict) -> None:
+        if hasattr(self, 'dict_of_exercises'):
+            raise Exception("Exercises are already set, setting them again "
+                            "would cause lost of data. "
+                            "Try other methods instead ie. add_one_exercise, "
+                            "add_multiple_exercises or remove_all_exercises.")
+        self._dict_of_exercises = dict_of_exercises
 
-        for exercise in exercises:
-            if not isinstance(exercise, Exercise):
-                raise Exception("All exercises must be Exercise class objects!")
-
-        self._exercises = exercises
-
-    # @dispatch(Exercise)
-    def add_exercise(self, exercise: Exercise) -> None:
+    def add_one_exercise(self, exercise: Exercise) -> None:
         if not isinstance(exercise, Exercise):
             raise Exception("Exercise must be Exercise class object!")
-        self.exercises.append(exercise)
+        if exercise.name in self.dict_of_exercises:
+            raise Exception("Exercise with this name already exists "
+                            "in current training!")
+        self.dict_of_exercises[exercise.name] = exercise
+
+    # def add_multiple_exercises(self, exercises: List[Exercise]) -> None:
+    #     if not isinstance(exercises, List):
+    #         raise Exception("Exercises must be list!")
+    #     for exercise in exercises:
+    #         self.add_one_exercise(exercise)
