ca4cbfe80101882032e005af788dd4834a94cb8e
Paweł <pawellamza@gmail.com>
2023-05-14 19:39:41 +0200

    added tests for expected_number_of_reps_in_each_set

--- tests/test_Exercise.py
+++ tests/test_Exercise.py
@@ -44,9 +44,38 @@ def test_too_big_number_of_exercise_sets(exercise):
     assert exercise.number_of_exercise_sets != number_of_exercise_sets
 
 
-def test_correct_number_of_exercise_sets(exercise):
-    number_of_exercise_sets = 4
-    exercise.number_of_exercise_sets = number_of_exercise_sets
+# def test_correct_number_of_exercise_sets(exercise):
+#     number_of_exercise_sets = 4
+#     exercise.number_of_exercise_sets = number_of_exercise_sets
+#
+#     assert exercise.number_of_exercise_sets == number_of_exercise_sets
+#     assert len(exercise.exercise_sets) == number_of_exercise_sets
 
-    assert exercise.number_of_exercise_sets == number_of_exercise_sets
-    assert len(exercise.exercise_sets) == number_of_exercise_sets
+def test_init_to_none_expected_number_of_reps_in_each_set():
+    name = "Pull ups"
+    number_of_sets = 3
+    exercise = Exercise(name, number_of_sets)
+    assert exercise.expected_number_of_reps_in_each_set is None
+
+
+def test_none_expected_number_of_reps_in_each_set(exercise):
+    with pytest.raises(Exception):
+        exercise.expected_number_of_reps_in_each_set = None
+
+
+def test_incorrect_expected_number_of_reps_in_each_set(exercise):
+    x = "wrong number"
+    y = 2.4124
+    z = -512
+    with pytest.raises(Exception):
+        exercise.expected_number_of_reps_in_each_set = x
+    with pytest.raises(Exception):
+        exercise.expected_number_of_reps_in_each_set = y
+    with pytest.raises(Exception):
+        exercise.expected_number_of_reps_in_each_set = z
+
+
+def test_correct_expected_number_of_reps_in_each_set(exercise):
+    x = 10
+    exercise.expected_number_of_reps_in_each_set = x
+    assert exercise.expected_number_of_reps_in_each_set == x
